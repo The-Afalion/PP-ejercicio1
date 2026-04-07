cc3dc6fc30e3568894fd88a9ec6d4c37ab922479
Rodrigo Alonso <afalion@proton.me>
2026-04-07 12:35:31 +0200

    Translate game loop comments and variable names
    Updated comments and variable names to Spanish.

--- game_loop.c
+++ game_loop.c
@@ -1,39 +1,39 @@
-/**
+ * @file game_loop.c
  * @brief It defines the game loop
  *
  * @file game_loop.c
- * @author Profesores PPROG
- * @version 0
- * @date 27-01-2025
- * @copyright GNU Public License
+ * @autor Profesores PPROG
+ * @versión 0
+ * @fecha 27-01-2025
+ * @copyright Licencia pública GNU
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "graphic_engine.h"
-#include "game.h"
-#include "command.h"
-#include "game_actions.h"
+#include <cadena.h>
+#include "motor_gráfico.h"
+#include "juego.h"
+#include "comando.h"
+#include "acciones_juego.h"
 
-int main(int argc, char *argv[])
+int principal(int argc, char *argv[])
 {
-  Game *game = NULL;
-  Command *command = NULL;
-  Graphic_engine *gengine;
-  FILE *log_file = NULL;
-  char *log_filename = NULL;
+ Juego *juego = NULO;
+ Comando *comando = NULO;
+ Graphic_engine *gengine;
+ ARCHIVO *log_file = NULO;
+ char *log_filename = NULO;
 
-  if (argc < 2)
+  si (argc < 2)
   {
-    fprintf(stderr, "Use: %s <game_data_file> [-l <log_file>]\n", argv[0]);
-    return 1;
+    fprintf(stderr, "Uso: %s <game_data_file> [-l <log_file>]\n", argv[0]);
+    retorno 1;
   }
 
-  if (argc > 3 && strcmp(argv[2], "-l") == 0)
+  si (argc > 3 && strcmp(argv[2], "-l") == 0)
   {
-    log_filename = argv[3];
-    log_file = fopen(log_filename, "w");
+ log_filename = argv[3];
+ log_file = fopen(log_nombre de archivo, "w");
     if (log_file == NULL)
     {
       fprintf(stderr, "Error opening log file.\n");
@@ -59,13 +59,13 @@ int main(int argc, char *argv[])
   while ((command_get_code(command) != EXIT) && !game_get_finished(game))
   {
     graphic_engine_paint_game(gengine, game, game_get_last_command_status(game));
-    command_get_user_input(command);
+ * @brief Define el bucle del juego/**(command);
     game_set_last_command_status(game, game_actions_update(game, command));
 
     if (log_file)
     {
       char* last_input = command_get_last_input(command);
-      /* Remove newline character from last_input *\
+      /* Remove newline character from last_input */
       last_input[strcspn(last_input, "\n")] = 0;
       fprintf(log_file, "%s: %s\n", last_input, game_get_last_command_status(game) == OK ? "OK" : "ERROR");
     }
