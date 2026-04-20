76ba017fe17478d9bacda73085c255d2a8a16a49
Rodrigo Alonso <afalion@proton.me>
2026-04-20 11:16:17 +0200

    script bash para los tests
    se ejecuta con: bash -lc PP-ejercicio1-main' && ./run_tests.sh 0

---
+++ run_tests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+mode="$1"
+test_elegido="${2:-}"
+
+if [[ -n "$test_elegido" ]]; then
+  make "$test_elegido"
+
+  if [[ "$mode" == "0" ]]; then
+    "./build/$test_elegido"
+  else
+    valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes "./build/$test_elegido"
+  fi
+else
+  make tests
+
+  for binary in build/*_test; do
+    if [[ "$mode" == "0" ]]; then
+      "./$binary"
+    else
+      valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes "./$binary"
+    fi
+    echo
+  done
+fi
