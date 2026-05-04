505c8e8b38d0b1572aaf06545c59fdfb96366a1a
unai <unai.gfga@gmail.com>
2026-05-04 11:54:45 +0200

    game_rules

--- src/game_rules.c
+++ src/game_rules.c
@@ -10,7 +10,31 @@
 
 #include "game.h"
 #include "game_rules.h"
+#include <stdio.h>
+#include <stdlib.h>
+#define SHERIFF_ID 70
 
-Status game_rules_update(Game *game, Command *cmd) {
+Id game_rules_sheriff_is_dead(Game * game){
+    int sheriffhp = 10;
+    Character * character = NULL;
+    if (!(character = game_get_character_from_id(game,SHERIFF_ID)))
+    {
+        return NO_ID;
+    }
+    sheriffhp = character_get_health(character);
+
+    if(sheriffhp <= 0)
+    {
+        return 1;
+    }
+}
+
+Status game_rules_update(Game *game, Command *cmd){
+
+    if (game_rules_sheriff_is_dead(game) == 1)
+    {
+        game_set_finished(game,-1);
+    }
     
 }
+
